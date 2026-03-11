77b4c73e1be75b270f5168d1da8aa80f503b84de
dalejoseph-dev <dalejoseph035@gmail.com>
2026-03-11 09:53:34 +0800

    Finished coding the project#3

---
+++ script.js
@@ -0,0 +1,16 @@
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate() {
+  const suffix = this.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+inputs.forEach(input => {
+  input.addEventListener('change', handleUpdate);
+  input.addEventListener('input', handleUpdate);
+  
+  if (input.type === 'range') {
+    input.addEventListener('mousemove', handleUpdate); 
+  }
+});
+
